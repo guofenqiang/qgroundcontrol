5de68c6f11216b01dfb9c05ce920c3593be69611
guofenqiang <2540640333@qq.com>
2024-02-19 16:02:57 +0800

    qgc: disable check version
    shield _checkForNewVersion fun
    
    Signed-off-by: guofenqiang <2540640333@qq.com>

--- src/QGCApplication.cc
+++ src/QGCApplication.cc
@@ -372,7 +372,7 @@ QGCApplication::QGCApplication(int &argc, char* argv[], bool unitTesting)
    }
 #endif /* __mobile__ */
 
-    _checkForNewVersion();
+//    _checkForNewVersion();
 }
 
 void QGCApplication::_exitWithError(QString errorMessage)
